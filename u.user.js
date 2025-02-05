4e205176d12827ab9351fea4f20014f5a5e4a0d5
Kitten Films <kittenapps@icloud.com>
2025-02-05 15:59:40 +0500

    Update u.user.js

--- u.user.js
+++ u.user.js
@@ -7,5 +7,7 @@
 // @match        https://www.geo-fs.com/geofs.php?v=3.9
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
 // @grant        none
+// @updateURL   https://github.com/KittenApps-Films/Geofs-fuel-addon/raw/refs/heads/main/u.user.js
+// @downloadURL https://github.com/KittenApps-Films/Geofs-fuel-addon/raw/refs/heads/main/u.user.js
 // ==/UserScript==
 console.log("run")
