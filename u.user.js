201c32b8b7b4aba3ec765abddb388b14673d2164
Kitten Films <kittenapps@icloud.com>
2025-02-05 15:50:14 +0500

    Update u.user.js

--- u.user.js
+++ u.user.js
@@ -1,2 +1,11 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
 // @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.geo-fs.com/geofs.php?v=3.9
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// @grant        none
+// ==/UserScript==
 console.log("run")
